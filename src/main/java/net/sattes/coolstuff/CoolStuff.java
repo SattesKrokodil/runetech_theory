d14fcb8abd6466030bb2ffdd64ff900edfbca138
Sattes <maximilian.zmek@gmail.com>
2022-02-17 10:33:43 +0100

    fixed some recipes, added test custom status effect, added bleed particles

--- src/main/java/net/sattes/coolstuff/CoolStuff.java
+++ src/main/java/net/sattes/coolstuff/CoolStuff.java
@@ -2,11 +2,14 @@ package net.sattes.coolstuff;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
+import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.particle.DefaultParticleType;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.sattes.coolstuff.item.ModItems;
+import net.sattes.coolstuff.status_effects.BleedStatusEffect;
 import net.sattes.coolstuff.util.ModRegistries;
+import net.sattes.coolstuff.util.StatusEffectRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,12 +20,12 @@ public class CoolStuff implements ModInitializer {
 	public static Identifier id(String path) {
 		return new Identifier(MOD_ID, path);
 	}
-	public static final DefaultParticleType GREEN_FLAME = FabricParticleTypes.simple();
+	public static final DefaultParticleType BLOOD_DRIPPING = FabricParticleTypes.simple();
 
 	@Override
 	public void onInitialize() {
-
-		Registry.register(Registry.PARTICLE_TYPE, new Identifier("coolstuff", "green_flame"), GREEN_FLAME);
+		Registry.register(Registry.PARTICLE_TYPE, new Identifier("coolstuff", "blood_dripping"), BLOOD_DRIPPING);
+		StatusEffectRegistry.registerStatusEffects();
 		ModRegistries.registerModStuffs();
 		ModItems.registerModItem();
 		LOGGER.info("Making stuff cooler ...");
---
+++ src/main/java/net/sattes/coolstuff/status_effects/BleedStatusEffect.java
@@ -0,0 +1,29 @@
+package net.sattes.coolstuff.status_effects;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectCategory;
+import net.minecraft.entity.player.PlayerEntity;
+
+public class BleedStatusEffect extends StatusEffect {
+    public BleedStatusEffect() {
+        super(
+                StatusEffectCategory.HARMFUL,
+                0x98D982); // color in RGB
+    }
+
+    // This method is called every tick to check whether it should apply the status effect or not
+    @Override
+    public boolean canApplyUpdateEffect(int duration, int amplifier) {
+        // In our case, we just make it return true so that it applies the status effect every tick.
+        return true;
+    }
+
+    // This method is called when it applies the status effect. We implement custom functionality here.
+    @Override
+    public void applyUpdateEffect(LivingEntity entity, int amplifier) {
+        if (entity instanceof PlayerEntity) {
+            ((PlayerEntity) entity).addExperience(1 << amplifier); // Higher amplifier gives you EXP faster
+        }
+    }
+}
