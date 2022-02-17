2034307a064b17a55a42a6208a1a749aea5ae3b8
Sattes <maximilian.zmek@gmail.com>
2022-02-17 23:58:56 +0100

    mixin kaputt

---
+++ src/main/java/net/sattes/coolstuff/mixin/MixinLivingEntity.java
@@ -0,0 +1,41 @@
+package net.sattes.coolstuff.mixin;
+
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+
+import net.minecraft.particle.ParticleTypes;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+
+import static net.sattes.coolstuff.CoolStuff.BLOOD_DRIPPING;
+import static net.sattes.coolstuff.util.StatusEffectRegistry.BLEED_STATUS_EFFECT;
+
+
+@Mixin(LivingEntity.class)
+public abstract class MixinLivingEntity {
+
+	@Shadow
+	@Final
+	private static TrackedData<Boolean> POTION_SWIRLS_AMBIENT;
+
+
+	@Inject(method = "tickStatusEffects", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;addParticle(Lnet/minecraft/particle/ParticleEffect;DDDDDD)V"), cancellable = true)
+	protected void tickStatusEffects(CallbackInfo ci) {
+		LivingEntity living = (LivingEntity) (Object) this;
+		DataTracker dataTracker = living.getDataTracker();
+
+		if(living.hasStatusEffect(BLEED_STATUS_EFFECT)) {
+			living.world.addParticle(dataTracker.get(POTION_SWIRLS_AMBIENT) ? ParticleTypes.ENTITY_EFFECT : BLOOD_DRIPPING, living.getParticleX(0.5D), living.getRandomBodyY(), living.getParticleZ(0.5D), 0, 0, 0);
+			ci.cancel();
+		}
+
+	}
+}
+
